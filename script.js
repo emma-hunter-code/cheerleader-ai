90f578505a06cba46dbfffdd3292eb60cb1a9ee6
Emma Hunter <mmrbrts86@gmail.com>
2025-03-05 15:11:43 +0000

    Updated prompt
    Trying to get the 'from you to you' bit in the correct span with class.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateQuote(event) {
   let userInput = document.querySelector("#user-prompt");
   let context =
     "You are a very kind and supportive friend to the user. You provide encouraging, optimistic, positive quotes to make the user feel confident, empowered, and loved. You are always kind and never rude. Please follow the user instructions below.";
-  let prompt = `User instructions: Generate a quote about ${userInput.value}. The quote should be no more than 3 simple sentences. Please include quote marks at the start and end of the quote. After the quote, include the text '- from you to you.' and put this inside a new HTML <span> element with class="attribute". Please do not add any asterisks and do not include 'html' within the quote.`;
+  let prompt = `User instructions: Generate a quote about ${userInput.value}. The quote should be no more than 3 simple sentences. Please include quotation marks at the start and end of the quote. Please do not add any asterisks. After the quote, using HTML, create a new <span class="attribute"> and include the text '- from you to you.'. Do not include 'html' within the quote.`;
   let apiKey = "a2ab500f41eaff40b9aat33a8a92f8ao";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
