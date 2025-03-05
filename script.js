de6f525e74ba0b587cc722a2f90425533153f09b
Emma Hunter <mmrbrts86@gmail.com>
2025-03-05 15:04:05 +0000

    Updated UX

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function displayQuote(response) {
-  console.log("generated");
   new Typewriter("#quote-box", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,14 +12,16 @@ function generateQuote(event) {
 
   let userInput = document.querySelector("#user-prompt");
   let context =
-    "You are a very kind and supportive friend to the user. You provide encouraging, optimistic, positive quotes to make the user feel confident, empowered, and loved. Please follow the user instructions below.";
-  let prompt = `User instructions: Generate a quote about ${userInput.value}. The quote should be no more than 3 simple sentences. Please provide this in basic HTML format. Please include quote marks at the start and end of the quote. After the quote, on a new line, please attribute the quote from '- from you to you.' and put this inside a <span> element with the class of 'attribute'. Please do not add any asterisks and do not include 'html' within the quote.`;
+    "You are a very kind and supportive friend to the user. You provide encouraging, optimistic, positive quotes to make the user feel confident, empowered, and loved. You are always kind and never rude. Please follow the user instructions below.";
+  let prompt = `User instructions: Generate a quote about ${userInput.value}. The quote should be no more than 3 simple sentences. Please include quote marks at the start and end of the quote. After the quote, attribute the quote from '- from you to you.' and put this inside a new HTML <span> element with the class of 'attribute'. Please do not add any asterisks and do not include 'html' within the quote.`;
   let apiKey = "a2ab500f41eaff40b9aat33a8a92f8ao";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let quoteElement = document.querySelector("#quote-box");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="material-symbols-outlined blink">
+more_horiz
+</div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
